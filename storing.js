68d122d2144459ee46386b2ca21191168c4595d0
rinkoo <rinkookandpal1966@gmail.com>
2022-11-20 15:21:33 +0530

    third commit

--- storing.js
+++ storing.js
@@ -1,17 +1,38 @@
-function addToCart(event){
-    const button=event.target;
-    const shopItem=button.parentElement;
-
-    const price=shopItem.getElementsByClassName('prod-details')[0].innerText;
-
-    const image=shopItem.getElementsByClassName('prod-images')[0].src;
-
-    const title=shopItem.getElementsByTagName("h3")[0].innerText;
-
-    addingToCard(price, image, title);
+window.onload = function () {
+    event.preventDefault();
+    axios.get('http://localhost:3000/products')
+        .then(data => showingOnScreen(data.data))
+        .catch(err => connsole.log(err));
 }
 
-function addingToCard(p,i,t){
+function showingOnScreen(YourProducts) {
+    for (let i = 0; i < YourProducts.length; i++) {
+        const main = document.getElementById('Product-content');
+        const mainChild = `<div id="plant1">
+        <h3>${YourProducts[i].title}</h3>
+        <div class="image-container">
+            <img class="prod-images"
+                src="${YourProducts[i].imageUrl}"
+                alt="">
+        </div>
+        <div class="prod-details">
+            <span>$ ${YourProducts[i].price}</span>
+        </div>
+        <button class="shop-item-button" type="button">ADD TO CART</button>
+    </div>`
+        main.innerHTML = main.innerHTML + mainChild;
+    }
+
+}
+function addToCart(event) {
+    const button = event.target;
+    const shopItem = button.parentElement;
+    const price = shopItem.getElementsByClassName('prod-details')[0].innerText;
+    const image = shopItem.getElementsByClassName('prod-images')[0].src;
+    const title = shopItem.getElementsByTagName("h3")[0].innerText;
+    addingToCard(price, image, title);
+}
+function addingToCard(p, i, t) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -31,42 +52,22 @@ function addingToCard(p,i,t){
 </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow);
-// ...............
+    // ........this is for notification.......
     const container = document.getElementById('plants');
-    const notify=document.createElement('div');
-    notify.classList.add('notification');
     const notification = document.createElement('div');
-    notify.appendChild(notification);
-    console.log(notify);
-    // notification.classList.add('notification');
+    notification.classList.add('notification');
     notification.innerHTML = `<h4>Your Product : <span>${t}</span> is added to the cart<h4>`;
-    container.appendChild(notify);
-    setTimeout(()=>{
-        notify.remove();
-    },2500);
-    // .............
-//     const cartItem=document.getElementsByClassName('cart-items');
-//     cartItem.innerHtml=`<div class="cart-row" id="in-cart-album1">
-//     <span class="cart-item cart-column">
-//         <img class="cart-img"
-//             src="${i}"
-//             alt="">
-//         <span>${t}</span>
-//     </span>
-//     <span class="cart-price cart-column">${p}</span>
-//     <span class="cart-quantity cart-column">
-//         <input type="text" value="1">
-//         <button>REMOVE</button>
-//     </span>
-// </div>`
-
+    container.appendChild(notification);
+    setTimeout(() => {
+        notification.remove();
+    }, 2500);
 }
 
-function ShowingCart(){
-    const id= document.getElementById('cart');
-    id.style="display: block";
+function ShowingCart() {
+    const id = document.getElementById('cart');
+    id.style = "display: block";
 }
-function CartCancel(){
-    const id1= document.getElementById('cart');
-    id1.style="display: none";
+function CartCancel() {
+    const id1 = document.getElementById('cart');
+    id1.style = "display: none";
 }
